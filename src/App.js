bc3a42260244e8ea394ca114fbf611c03a13785c
Ryan White <ryanwhite92@protonmail.com>
2024-06-20 22:43:56 -0700

    List liked form submissions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from "@mui/material/Container";
 
 import Header from "./Header";
 import Content from "./Content";
 import Toast from "./Toast";
+import { fetchLikedFormSubmissions } from "./service/mockServer";
 
 function App() {
+  const [likedSubmissions, setLikedSubmissions] = useState([]);
+
+  useEffect(() => {
+    // TODO: handle failures
+    fetchLikedFormSubmissions()
+      .then((resp) => {
+        console.log(resp);
+        setLikedSubmissions(resp.formSubmissions);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <>
       <Header />
       <Container>
-        <Content />
+        <Content likedSubmissions={likedSubmissions} />
         <Toast />
       </Container>
     </>
--- src/Content.js
+++ src/Content.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { List, ListItem, ListItemText } from "@mui/material";
 
-export default function Content() {
+export default function Content({ likedSubmissions }) {
   return (
-    <Box sx={{marginTop: 3}}>
+    <Box sx={{ marginTop: 3 }}>
       <Typography variant="h4">Liked Form Submissions</Typography>
 
-      <Typography variant="body1" sx={{fontStyle: 'italic', marginTop: 1}}>
-        TODO: List of liked submissions here (delete this line)
-      </Typography>
+      <List>
+        {likedSubmissions.map(({ id, data }) => (
+          <ListItem disablePadding key={id}>
+            <ListItemText
+              primary={`${data.firstName} ${data.lastName} ${data.email}`}
+            />
+          </ListItem>
+        ))}
+      </List>
     </Box>
   );
 }
